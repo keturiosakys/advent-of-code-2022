e4a3204ef535675c21697cb5e6513af19198ba94
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2022-12-05 21:54:13 +0100

    cleanup

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,28 +1,3 @@
-// 1. Split by lines
-// 2. Split each line in halg
-// 3. Find overlapping chars
-// 4. Push them to an array
-// 5. Find a corresponding priorities
-// 6. Sum them
-//
-//
-// @ Bindings might be helpful here
-//     enum Message {
-//     Hello { id: i32 },
-//    }
-//
-// let msg = Message::Hello { id: 5 };
-//
-// match msg {
-//     Message::Hello {
-//         id: id_variable @ 3..=7,
-//     } => println!("Found an id in range: {}", id_variable),
-//     Message::Hello { id: 10..=12 } => {
-//         println!("Found an id in another range")
-//     }
-//     Message::Hello { id } => println!("Found some other id: {}", id),
-// }
-
 #[derive(Debug)]
 struct Priority {
     character: Vec<char>,
